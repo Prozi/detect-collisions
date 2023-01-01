492c13de6c347b521ebfba9e1c7eb851ae9a4f88
Jacek Pietal <jacekp@pop-os.localdomain>
2023-01-02 00:55:02 +0100

    feat: improve types for poly-decomp

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,5 @@
 declare module "poly-decomp" {
   function quickDecomp(points: number[][]): number[][][];
+  function decomp(points: number[][]): number[][][];
+  function isSimple(points: number[][]): boolean;
 }
