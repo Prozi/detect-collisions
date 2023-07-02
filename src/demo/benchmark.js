4abb4fe142a87582643c022fd8e892bc01fc45dc
Jacek Pietal <jacekp@pop-os.localdomain>
2023-07-02 04:00:41 +0200

    feat: update benchmark

--- src/demo/benchmark.js
+++ src/demo/benchmark.js
@@ -6,17 +6,23 @@ let test;
 
 const bench = new Bench({
   time: 1000,
+  warmupIterations: 0,
   setup: ({ opts }) => {
-    if (test) {
-      test.physics.clear();
-    }
-
     test = new Stress(opts.items);
+  },
+  teardown: () => {
+    test.physics.clear();
   },
 });
 
 const recursiveAddTest = (items) => {
-  bench.add(`stress test, items=${items}`, () => test.update(), { items });
+  bench.add(
+    `stress test, items=${items}`,
+    () => {
+      test.update();
+    },
+    { items }
+  );
 
   if (items < 10000) {
     recursiveAddTest(items + 1000);
