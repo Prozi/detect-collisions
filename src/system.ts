c2379592408b9d90db59fac7a249457705adc3a2
noih.dev <noih.dev@gmail.com>
2024-04-21 11:59:10 +0800

    feat: check collisions within the area

--- src/system.ts
+++ src/system.ts
@@ -6,6 +6,7 @@ import {
   intersectLinePolygon,
 } from "./intersect";
 import {
+  BBox,
   Body,
   BodyType,
   CollisionCallback,
@@ -91,6 +92,22 @@ export class System<TBody extends Body = Body> extends BaseSystem<TBody> {
     };
 
     return some(bodies, checkCollision);
+  }
+
+  /**
+   * check all bodies in area collisions with callback
+   */
+  checkArea(
+    area: BBox,
+    callback: CollisionCallback = returnTrue,
+    response = this.response,
+  ): boolean {
+    const bodies = this.search(area);
+    const checkOne = (body: TBody) => {
+      return this.checkOne(body, callback, response);
+    };
+
+    return some(bodies, checkOne);
   }
 
   /**
